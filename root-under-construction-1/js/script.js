dce995b6501f194a55d976295d8a4a582437cc1e
Calin Cionca <36063669+calincionca35@users.noreply.github.com>
2018-11-20 15:30:20 -0800

    Add files via upload

---
+++ root-under-construction-1/js/script.js
@@ -0,0 +1,32 @@
+//set countdown date
+var countDownDate = new Date("Nov 20, 2019 14:49:01").getTime();
+
+//update countdown every second
+var countdown = setInterval(function() {
+    "use strict";
+    //get today date and time
+    var today = new Date().getTime();
+
+    //find interval between today and countdown date
+    var interval = countDownDate - today;
+
+    //calculate days, hours, minutes and seconds to countdown date
+    var days = Math.floor(interval / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((interval % (1000 * 60)) / 1000);
+
+    //display results
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    //when countdown ends, write "Under Construction"
+    if (interval < 0) {
+        clearInterval(countdown);
+        document.getElementById("wrapper").innerHTML = "Under Construction";
+        document.getElementById("wrapper").style.color = "#ff0";
+        document.getElementById("wrapper").style.fontSize = "2.25em";
+    }
+}, 1000);
