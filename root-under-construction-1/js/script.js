07836f427cbf9590abebaa2d8486353ff3a9b419
Calin <36063669+calincionca35@users.noreply.github.com>
2024-11-17 18:50:13 -0800

    Update script.js

--- root-under-construction-1/js/script.js
+++ root-under-construction-1/js/script.js
@@ -4,7 +4,7 @@ var $ = function(id) {
 };
 
 //set countdown date
-var countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+var countDownDate = new Date("Jan 1, 2025 00:00:00").getTime();
 
 //update countdown every second
 var countdown = setInterval(function() {
